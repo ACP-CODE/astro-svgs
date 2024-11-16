466161a37743d5ba76abc16a27468eccbd696040
Jaki Chen <JakiChen@live.cn>
2024-11-17 00:02:10 +0800

    Update helpers.ts
    - `md5` 箭头函数
    - 美化更名为 `format`，及升级了部分功能

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,9 +1,8 @@
 import type { Precision } from ".";
 import { createHash } from "crypto";
 
-export function md5(content: string): string {
-  return createHash("md5").update(content).digest("hex").slice(0, 8);
-}
+export const md5 = (content: string): string =>
+  createHash("md5").update(content).digest("hex").slice(0, 8);
 
 export function minify(content: string, level: Precision): string {
   content = content.replace(/\s{2,}/g, " ");
@@ -25,6 +24,16 @@ export function minify(content: string, level: Precision): string {
   return content;
 }
 
-export function beautify(content: string): string {
-  return content.replace(/></g, ">\n<");
+export function format(content: string): string {
+  return content
+    .replace(/(<defs>|<\/defs>)/g, "\n\t$1")
+    .replace(/(<symbol[^>]*>|<\/symbol>)/g, "\n\t\t$1")
+    .replace(/(<path[^>]*>|<polygon[^>]*>|<ellipse[^>]*>)/g, "\n\t\t\t$1")
+    .replace(/(<path[^>]*>|<polygon[^>]*>|<ellipse[^>]*>)/g, (match) =>
+      match.replace(/\s+/g, " ").replace("\n", " "),
+    )
+    .replace(/(<\/svg>)/g, "\n$1")
+    .split("\n")
+    .filter((line) => line.trim() !== "")
+    .join("\n");
 }
