c4e23653473443d703451e756ad05edbecb7d4da
Jaki Chen <JakiChen@live.cn>
2024-11-19 15:02:53 +0800

    Update index.ts
    ## issues
    
    #1 #5 #8 #7

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,56 @@
 import type { AstroIntegration } from "astro";
 import { defaults } from "./core";
 import { create } from "./plugin";
+import { virtual } from "./modules";
 
 export const name = "astro-svgs";
 
 export interface SVGsOptions {
+  /**
+   * Specify the folder where the svg file is located.
+   * @default "src/svgs"
+   */
   input?: string | string[];
+  /**
+   * @default "high"
+   * @example Dynamic setup based on environment
+   * ```ts
+   * compress: isDev ? 'beautify' : 'high',
+   * ```
+   */
   compress?: Precision;
 }
 
 export type Precision = "low" | "medium" | "high" | "beautify";
 
+export class Error1 extends Error {
+  hint?: string;
+
+  constructor(message: string) {
+    super(message);
+    this.name = `[${name}]`;
+
+    // 捕获堆栈信息，确保错误信息和堆栈正确显示
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, Error1);
+    }
+  }
+}
+
 export default function svgs(options?: SVGsOptions): AstroIntegration {
   const opts = { ...defaults, ...options };
-
   return {
     name,
     hooks: {
-      "astro:config:setup": async ({ config, updateConfig, logger }) => {
+      "astro:config:setup": async ({ config, updateConfig }) => {
         updateConfig({
           vite: {
-            plugins: [create(opts, config, logger)],
+            plugins: [create(opts, config)],
           },
         });
+      },
+      "astro:config:done": async ({ injectTypes }) => {
+        injectTypes(await virtual(opts));
       },
     },
   };
